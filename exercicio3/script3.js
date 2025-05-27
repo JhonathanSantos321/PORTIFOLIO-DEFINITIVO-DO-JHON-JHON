617c2f29b716aefcf9b80b459c29a523eb75e673
JhonathanSantos321 <jhonathan.demarch@escola.pr.gov.br>
2025-05-27 12:26:27 -0300

    .

--- exercicio3/script3.js
+++ exercicio3/script3.js
@@ -1,23 +1,33 @@
+let nomealuno;
+
+nomealuno = (window.prompt("digite aqui seu nome"));
+
+let resultado
+alert("seu nome é " + nomealuno + ".")
+
 var nota1 = Number(window.prompt("digite sua nota: "));
 var nota2 = Number(window.prompt("digite sua 2 nota: "));
 var nota3 = Number(window.prompt("digite sua 3 nota: "));
 
 
 let total = (nota1 + nota2 + nota3)/3;
-
-
-
-
-
-alert("media final " + total)
+alert("media final " + total);
 
 if(total >= 60){
-    alert(" voce atingiu a media " + total )
-    alert(" aprovado ")
-}else{
-    pontosfaltando == 60 - totalalert("reprovado faltaram" + pontosfaltando + " pontos ")
-
+    alert(" voce atingiu a media " + total );
+    alert( + nomealuno + " aprovado ");
+}else {
+    alert("reprovado faltaram" + pontosfaltando + " pontos ");
 }
+
+
+
+
+
+
+
+
+
 
 
   
