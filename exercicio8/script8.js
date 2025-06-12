99c362d60a71e85d2d1807b60b0da703b1b0d331
JhonathanSantos321 <jhonathan.demarch@escola.pr.gov.br>
2025-06-12 10:49:32 -0300

    .

---
+++ exercicio8/script8.js
@@ -0,0 +1,15 @@
+let numSecreto;
+let maioroumenor;
+
+
+function aleatorio(){
+
+num1 = Number(window.prompt("digite um numero"));
+
+if(maioroumenor = num1 > num2){
+    alert("o número " + num1 +" é maior");
+}else{
+    alert("o número " + num2 + " é maior");
+}
+
+}
