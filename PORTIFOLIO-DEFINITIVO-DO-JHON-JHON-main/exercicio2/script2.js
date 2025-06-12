e1f91cfbf36fe076c4ad28607c3516463d084b75
JhonathanSantos321 <jhonathan.demarch@escola.pr.gov.br>
2025-06-12 08:37:08 -0300

    .

---
+++ PORTIFOLIO-DEFINITIVO-DO-JHON-JHON-main/exercicio2/script2.js
@@ -0,0 +1,31 @@
+function somar(){
+    let  num1 = Number(window.prompt("digite aqui o primeiro numero"));
+    let  num2 = Number(window.prompt("digite aqui o segundo numero"));
+
+    let resultado = num1 + num2 ;
+    alert("O resultado da soma é " + resultado + ".")
+}
+
+function subtrair(){
+    let  num1 = Number(window.prompt("digite aqui o primeiro numero"));
+    let  num2 = Number(window.prompt("digite aqui o segundo numero"));
+
+    let resultado = num1 - num2
+     alert("O resultado da Subtraçao é " + resultado + ".")
+}
+
+function dividir(){
+     let  num1 = Number(window.prompt("digite aqui o primeiro numero"));
+    let  num2 = Number(window.prompt("digite aqui o segundo numero"));
+
+    let resultado = num1 / num2
+     alert("O resultado da divisao é " + resultado + ".")
+}
+
+function multiplicar(){
+     let  num1 = Number(window.prompt("digite aqui o primeiro numero"));
+    let  num2 = Number(window.prompt("digite aqui o segundo numero"));
+
+    let resultado = num1 * num2
+    alert("O resultado da Multiplicaçao é " + resultado + ".")
+}
