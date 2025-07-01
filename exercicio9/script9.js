410dbad46b9b4c7c68a95ba05b7ebeb861497369
JhonathanSantos321 <jhonathan.demarch@escola.pr.gov.br>
2025-07-01 10:12:41 -0300

    .

--- exercicio9/script9.js
+++ exercicio9/script9.js
@@ -6,17 +6,19 @@ function cliques(){
     let estacao
     let cor
 
-    if (meses === "DEZEMBRO") || meses === "JANEIRO" || meses === "FEVEREIRO"){
+    if (meses === "DEZEMBRO" || meses === "JANEIRO" || meses === "FEVEREIRO"){
         estacao = "verao"
         cor = "#"
-    }else  if (meses === "MARÇO") || meses === "ABRIL" || meses === "MAIO") {
+    }else  if (meses === "MARÇO" || meses === "ABRIL" || meses === "MAIO") {
         estacao = "Outono"
-    }else  if (meses === "JUNHO", meses === "JULHO", meses === "AGOSTO" ) {
+    }else  if (meses === "JUNHO" || meses === "JULHO" || meses === "AGOSTO" ) {
         estacao = "Inverno"
-    }else  if (meses === "SETEMBRO") || meses === "OUTUBRO" || meses === "NOVEMBRO") {
+    }else  if (meses === "SETEMBRO" || meses === "OUTUBRO" || meses === "NOVEMBRO") {
         estacao = "Primavera"
 
     }
+
+    mostraHTML.innerHTML = `<p>O mes ${meses} pertence a estaçao: ${estacao} </p>`
 
     }
 
