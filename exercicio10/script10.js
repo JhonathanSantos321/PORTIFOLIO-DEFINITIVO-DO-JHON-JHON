1c3daf1df09a5652bc273ac389395240a7fa1b57
JhonathanSantos321 <jhonathan.demarch@escola.pr.gov.br>
2025-07-01 12:32:04 -0300

    .

--- exercicio10/script10.js
+++ exercicio10/script10.js
@@ -1,16 +1,24 @@
 function contar(){
      let mostraHTML = document.getElementById("mostraHTML");
-
      let contador = 1;
 
-     while (contador <= 10) {
-
-        mostraHTML.innerHTML += `<p> ${contador}  &#128524; </p>`
-        contador++;
-
-     }
-
-        
-
      
-}
+
+     while (contador <= 10) {
+      if (contador % 2 === 0) { 
+                    mostraHTML.innerHTML += `<b> <p> ${contador} </p></b>`;  
+                } else {
+                    mostraHTML.innerHTML += `<p> ${contador} </p>`; 
+                }
+                contador++;
+            }
+
+
+        }
+
+
+
+
+   
+
+  
