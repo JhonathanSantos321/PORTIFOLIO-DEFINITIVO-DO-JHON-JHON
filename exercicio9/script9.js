e22bb6fea00737c8cbde278989a27db3097d5d7a
JhonathanSantos321 <jhonathan.demarch@escola.pr.gov.br>
2025-07-01 08:47:52 -0300

    .

--- exercicio9/script9.js
+++ exercicio9/script9.js
@@ -1,36 +1,25 @@
 let mostraHTML = document.getElementById("mostraHTML");
-let text
-let OUTONO = ("ABRIL","MAIO","JUNHO")
-let VERAO = ("DEZEMBRO","JANEIRO","FEVEREIRO","MARCO")
-let PRIMAVERA = ("OUTUBRO,NOVEMBRO")
-let INVERNO  = ("JULHO","AGOSTO","SETEMBRO")
 
 
-function dias(){
-    let frase
+function cliques(){
+    let meses = prompt("Digite um mes: ").toUpperCase();
+    let estacao
+    let cor
 
-    let text = prompt("Digite um mes").ToUpperCase();
+    if (meses === "DEZEMBRO") || meses === "JANEIRO" || meses === "FEVEREIRO"){
+        estacao = "verao"
+        cor = "#"
+    }else  if (meses === "MARÇO") || meses === "ABRIL" || meses === "MAIO") {
+        estacao = "Outono"
+    }else  if (meses === "JUNHO", meses === "JULHO", meses === "AGOSTO" ) {
+        estacao = "Inverno"
+    }else  if (meses === "SETEMBRO") || meses === "OUTUBRO" || meses === "NOVEMBRO") {
+        estacao = "Primavera"
 
-     frase = mostraHTML.innerHTML += `<p>Voce Esta no${num_adiv}.</p>`
-
-     console.log(text);
-
-    text = ("MARCO","DEZEMBRO","SETEMBRO","JUNHO","JANEIRO","FEVEREIRO","MAIO","AGOSTO","OUTUBRO","NOVEMBRO","ABRIL","JULHO");
-
-    console.log(text)
-   
-
-    if(text = OUTONO){
-        mostraHTML.innerHTML = `<p>Voce Esta no ${OUTONO}.</p>`
-    }
-    if(text = VERAO){
-        mostraHTML.innerHTML = `<p>Voce Esta no ${VERAO}.</p>`
-    }
-    if(text = INVERNO){
-        mostraHTML.innerHTML = `<p>Voce Esta no ${INVERNO}.</p>`
-    }
-    if(text = PRIMAVERA){
-        mostraHTML.innerHTML = `<p>Voce Esta no ${PRIMAVERA}.</p>`
     }
 
-}
+    }
+
+
+
+    
